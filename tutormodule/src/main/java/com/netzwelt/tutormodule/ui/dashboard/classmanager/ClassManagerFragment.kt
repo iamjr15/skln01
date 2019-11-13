8a755d4f6f1cb597f87e47232869bed3c1baaded
Prithipal Thakur <prithipal.thakur@vtnetzwelt.com>
2019-11-13 18:21:34 +0530

    Tutor 1> Setup tabs for myrequest Screen.

---
+++ tutormodule/src/main/java/com/netzwelt/tutormodule/ui/dashboard/classmanager/ClassManagerFragment.kt
@@ -0,0 +1,24 @@
+package com.netzwelt.tutormodule.ui.dashboard.classmanager
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.netzwelt.tutormodule.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ClassManagerFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_class_manager, container, false)
+    }
+
+
+}
